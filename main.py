c585d76e6b80a056491796a2ea96971b676224ee
speedyconzales <9094731+speedyconzales@users.noreply.github.com>
2025-01-14 22:35:29 +0100

    rearrange order of hosters (#43)

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def main():
 
     os.makedirs(output_path, exist_ok=True)
 
-    providers = ["Vidoza", "VOE", "Doodstream", "Streamtape", "Vidmoly", "SpeedFiles"] if not provider else provider
+    providers = ["Vidmoly", "SpeedFiles", "Doodstream", "Streamtape", "Vidoza", "VOE"] if not provider else provider
 
     for season in seasons:
         season_path = f"{output_path}/Season {season:02}"
